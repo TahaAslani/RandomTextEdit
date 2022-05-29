5fe0c0c2cd5e023172fe8c111445ce132ff62859
Taha ValizadehAslani <47432410+TahaAslani@users.noreply.github.com>
2022-05-29 14:12:08 -0400

    Create noise.py

---
+++ noise.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+import random
+import os
+
+model_path = '/ifs/groups/liangGrp/tv349/PharmaBERT/DailyMed/InitialModel/bert-base-cased'
+random_seed = 42
+
+vocab_file = os.path.join(model_path,'vocab.txt')
+f = open(vocab_file,'r')
+words = f.readlines()
+f.close()
+
+np.random.seed(random_seed)
+
+def generate_random_wrod():
+    found_word = False
+    while not found_word:
+        random_number = np.random.randint(0,len(words))
+        word = words[random_number]
+        word = word.replace('\n', '')
+        if not word.startswith('##'):
+            found_word = True
+    return word
+
+def delete_random_words(text, probability):
+
+    text_split = text.split()
+    random_mask = np.random.choice(a=[False, True], size=(len(text_split)), \
+                                   p=[probability, 1-probability])
+    ret = [token for i, token in enumerate(text_split) if random_mask[i]]
+
+    if len(ret)>0:
+        output = " ".join(ret)
+    else:
+        output = " " # If everything is deleted, only return a space
+    return output 
+
+def replace_random_words(text, probability):
+    
+    text_split = text.split()
+    random_array = np.random.choice(a=[False, True], size=(len(text_split)), \
+                               p=[1-probability, probability])
+    for i,replace in enumerate(random_array):
+        if replace:
+            text_split[i] = generate_random_wrod()
+    return " ".join(text_split)
+
+def insert_random_words(text, probability):
+    
+    text_split = text.split()
+
+    i = 0
+    while i<= len(text_split):
+    
+        replace = np.random.choice(a=[False, True], p=[1-probability, probability])
+        
+        if replace:
+            text_split = text_split[:i] + [generate_random_wrod()] + text_split[i:]
+            
+        print(i)
+        
+    return " ".join(text_split)
+    
